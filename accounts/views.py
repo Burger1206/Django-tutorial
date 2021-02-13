e67c48da00cdb9db1afd9e14e61747312f58735b
Burger1206 <baseball.boy1206@me.com>
2021-02-13 14:40:47 +0900

    django-blog

--- accounts/views.py
+++ accounts/views.py
@@ -5,3 +5,14 @@ from allauth.account import views
 
 class LoginView(views.LoginView):
     template_name = 'accounts/login.html'
+
+class LogoutView(views.LogoutView):
+    template_name = 'accounts/logout.html'
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            self.logout()
+        return redirect('/')
+
+class SignupView(views.SignupView):
+    template_name = 'accounts/signup.html'
